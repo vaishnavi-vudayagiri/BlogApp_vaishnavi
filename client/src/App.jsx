a1f15f9c776826c21ab8577f3753a3f1c9c96d05
Vaishnavi Vudayagiri <vaishnavi@gmail.com>
2026-05-15 15:02:47 +0530

    vaishnavi

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import AuthorProfile from "./components/AuthorProfile";
 import AuthorArticles from "./components/AuthorArticles";
 import EditArticle from "./components/EditArticle";
 import WriteArticles from "./components/WriteArticles";
-import ArticleByID from "./components/ArticleByID";
+import ArticleByID from "./components/ArticleById";
 import { Toaster } from "react-hot-toast";
 import Unauthorized from "./components/Unauthorized";
 import ProtectedRoute from "./components/ProtectedRoute";
